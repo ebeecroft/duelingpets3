0b61c2bf5ebd1690b2187f818ca8747e60de6afb
Eric <ebeecroft@mail.csuchico.edu>
2016-07-11 21:45:30 -0700

    Fixed routes again

--- Trial/config/routes.rb
+++ Trial/config/routes.rb
@@ -11,6 +11,7 @@ Trial::Application.routes.draw do
       resources :sbooks #Builds everything
       resources :mainfolders #Builds everything
       resources :blogs #Builds everything
+      resources :pets, :only =>[:new, :create, :edit, :update]
    end
 
    #Builds the blogs
@@ -90,7 +91,7 @@ Trial::Application.routes.draw do
    post 'pets/review2' => 'pets#deny'
 
    #Base pets route
-   resources :pets #Builds everything for pets
+   resources :pets, :only => [:index, :show] #Builds everything for pets
 
    #Builds the item actions
    get '/items/maintenance' => 'items#maintenance'
