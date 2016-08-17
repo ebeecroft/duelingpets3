5498c21da18b38715886521236315c645810884a
Eric <ebeecroft@mail.csuchico.edu>
2016-08-16 21:05:37 -0700

    Added item mailer attributes to github

--- Trial/app/mailers/item_mailer.rb
+++ Trial/app/mailers/item_mailer.rb
@@ -1,3 +1,28 @@
 class ItemMailer < ActionMailer::Base
-  default from: "from@example.com"
+   default from: "notification@duelingpets.net"
+
+   def review_item(item)
+      @item = item
+      @url = "http://www.duelingpets.net/items/review"
+      allUsers = Usertype.all
+      reviewers = allUsers.select{|user| user.privilege == "Reviewer"}
+      if(reviewers.count > 0)
+         reviewers.each do |reviewer|
+            mail(to: reviewer.user.email, subject: "New Item Awaiting Review")
+         end
+      end
+   end
+
+   def item_approved(item, points)
+      @item = item
+      @points = points
+      @url = "http://www.duelingpets.net/items/#{@item.name}"
+      mail(to: item.user.email, subject: "Your Item was Approved")
+   end
+
+   def item_denied(item)
+      @item = item
+      @url = "http://www.duelingpets.net/items/#{@item.name}/edit"
+      mail(to: item.user.email, subject: "Your Item was Denied")
+   end
 end
